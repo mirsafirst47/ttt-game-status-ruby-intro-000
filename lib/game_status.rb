52e3340c34429a1078cd6bff9158048376e36bf0
Samir Triande <habib.triande@gmail.com>
2020-04-23 16:10:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,30 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # bottom row
+  [0,3,6], # Left vertical row
+  [1,4,7], # middle vertical row
+  [2,5,8], # Right vertical row
+  [0,4,8], # first diagonal row
+  [2,4,6] # second diagonal row
+]
+
+def won?(board)
+
+end
+
+def full?(board)
+
+end
+
+def draw?(board)
+end
+
+def over?(board)
+end
+
+def winner(board)
+end
